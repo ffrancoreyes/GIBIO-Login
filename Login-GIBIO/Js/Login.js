7a18a985ea1278000308307d6ee6e2caa424ff49
Fabrizzio Franco Reyes <ffrancoreyes@frba.utn.edu.ar>
2023-03-13 18:36:33 -0300

    Diseño preliminar del login de GIBIO UTNFRBA

---
+++ Login-GIBIO/Js/Login.js
@@ -0,0 +1,14 @@
+
+
+//efecto para que se mueva los paneles de GIBIO
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
